71ddda6f4b442f7c47f82afdc0b5ed7dca78bfe2
gofpattern <https://github.com/gofpattern/Experiments.git>
2015-08-10 16:32:07 -0400

    Initial design for the bookMark App

---
+++ swingUI/src/bookmark/controllers/BookmarkController.java
@@ -0,0 +1,22 @@
+package bookmark.controllers;
+
+import bookmark.model.BookMark;
+
+public class BookmarkController {
+	
+	public BookmarkController(BookMark bookmark) {
+		
+		this.bookmark = bookmark;
+	}
+
+	BookMark bookmark;
+	
+	
+	
+	public void addBookMark(){
+		
+	}
+
+	
+
+}
