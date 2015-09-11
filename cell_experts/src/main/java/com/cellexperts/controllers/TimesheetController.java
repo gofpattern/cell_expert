396e5bc74bd0f7ddf4456d58e8eabe92b712f35c
gofpattern <gofpattern@gmail.com>
2015-09-11 15:36:51 -0400

    jsp template and UI related files

--- cell_experts/src/main/java/com/cellexperts/controllers/TimesheetController.java
+++ cell_experts/src/main/java/com/cellexperts/controllers/TimesheetController.java
@@ -7,12 +7,12 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class TimesheetController {
 	
-	@RequestMapping("/welcome")
+	@RequestMapping("/timesheet")
 	public ModelAndView helloWorld() {
  
 		String message = "<br><div style='text-align:center;'>"
 				+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
-		return new ModelAndView("welcome", "message", message);
+		return new ModelAndView("Timesheet");
 	}
 
 }
